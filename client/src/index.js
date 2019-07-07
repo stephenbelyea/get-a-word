aecd0cf28c2fba48e74275052caa768fa701b212
Stephen Belyea <belyea.stephen@gmail.com>
2019-07-06 22:50:52 -0400

    Stripped down react-app for client side

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function App(props) {
+  return (
+    <div>foo</div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
