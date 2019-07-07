1ff7de443d66ca961ec84bf5c0635a67687df7d6
Stephen Belyea <belyea.stephen@gmail.com>
2019-07-07 09:41:51 -0400

    Toggle definitions visibility

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,9 +6,11 @@ class App extends React.Component {
     this.state = {
       term: "",
       definitions: [],
+      showDefinitions: false,
       loading: false
     };
     this.handleGetWord = this.handleGetWord.bind(this);
+    this.toggleShowDefinitions = this.toggleShowDefinitions.bind(this);
   }
 
   async getTermAndDefinitions() {
@@ -31,34 +33,59 @@ class App extends React.Component {
   }
 
   handleGetWord() {
+    if (this.state.showDefinitions) {
+      this.toggleShowDefinitions();
+    }
     this.getTermAndDefinitions();
   }
 
+  toggleShowDefinitions() {
+    this.setState(state => ({ showDefinitions: !state.showDefinitions }));
+  }
+
   render() {
-    const { term, definitions, loading } = this.state;
+    const { term, definitions, showDefinitions, loading } = this.state;
     return (
       <main>
         <div className="get-word">
           <h1>Get A Word</h1>
-          <button onClick={this.handleGetWord} disabled={loading}>
+          <button
+            className="get-word-button"
+            onClick={this.handleGetWord}
+            disabled={loading}
+          >
             Get Word!
           </button>
         </div>
         {term && !loading && (
           <div className="got-word">
             <h2>{term}</h2>
-            <ul>
-              {definitions.map(def => (
-                <li key={def.id}>
-                  {def.type && (
-                    <span className="type">
-                      (<em>{def.type}</em>){" "}
-                    </span>
-                  )}
-                  <span className="value">{def.value}</span>
-                </li>
-              ))}
-            </ul>
+            {definitions.length > 0 && (
+              <>
+                <p>
+                  <button
+                    className="toggle-definitions-button"
+                    onClick={this.toggleShowDefinitions}
+                  >
+                    {showDefinitions ? "Hide" : "Show"} definition(s)
+                  </button>
+                </p>
+                {showDefinitions && (
+                  <ul>
+                    {definitions.map(def => (
+                      <li key={def.id}>
+                        {def.type && (
+                          <span className="type">
+                            (<em>{def.type}</em>){" "}
+                          </span>
+                        )}
+                        <span className="value">{def.value}</span>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </>
+            )}
           </div>
         )}
       </main>
