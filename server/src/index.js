4f4f95cf8ec1bf9179d649a5112eccb4d51ec96c
Stephen Belyea <belyea.stephen@gmail.com>
2019-07-07 09:01:05 -0400

    Cors middleware for server get

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const { getTermAndDefinitions } = require('./term-and-definitions');
+const express = require("express");
+const cors = require("cors");
+const { getTermAndDefinitions } = require("./term-and-definitions");
 
 const app = express();
 
 app.listen(3001, () => {
-  console.log('Server running on port 3001');
+  console.log("Server running on port 3001");
 });
 
-app.get('/term-and-definitions', async (req, res, next) => {
+app.get("/term-and-definitions", cors(), async (req, res, next) => {
   try {
     const termAndDefinitions = await getTermAndDefinitions();
     res.json(termAndDefinitions);
   } catch (e) {
     res.status(500).send({ error: e });
   }
- });
+});
