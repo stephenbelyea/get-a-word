d122953f1d5a804b743bdbddb608ba1d3899e5b1
Stephen Belyea <belyea.stephen@gmail.com>
2019-07-07 09:01:20 -0400

    Return id for definition items

--- server/src/term-and-definitions.js
+++ server/src/term-and-definitions.js
@@ -1,29 +1,30 @@
-const rp = require('request-promise');
-const cheerio = require('cheerio');
+const rp = require("request-promise");
+const cheerio = require("cheerio");
 
 function getTermFromCheerioObj($) {
-  return $('h1#headword').attr('title');
+  return $("h1#headword").attr("title");
 }
 
-function getTypeAndValueFromDefinitionString(definition) {
-  const splitDef = definition.split('   ');
+function getTypeAndValueFromDefinitionString(definition, idx) {
+  const splitDef = definition.split("   ");
   if (!splitDef || splitDef.length !== 2) {
     return null;
   }
   return {
+    id: idx + 1,
     type: splitDef[0].trim(),
     value: splitDef[1].trim()
-  }
+  };
 }
 
 function getDefinitionsFromCheerioObj($) {
   const definitions = [];
-  $('#define h3 + ul > li').each(function() {
-    const definition = getTypeAndValueFromDefinitionString($(this).text());
+  $("#define h3 + ul > li").each(function(idx) {
+    const definition = getTypeAndValueFromDefinitionString($(this).text(), idx);
     if (definition) {
       definitions.push(definition);
     }
-  })
+  });
   return definitions;
 }
 
@@ -37,10 +38,10 @@ function getTermAndDefinitionsFromScrapeHtml(scrapeHtml) {
 
 async function getTermAndDefinitions() {
   try {
-    const scrapeHtml = await rp('https://www.wordnik.com/randoml');
+    const scrapeHtml = await rp("https://www.wordnik.com/randoml");
     return getTermAndDefinitionsFromScrapeHtml(scrapeHtml);
   } catch (e) {
-    return Promise.reject('Could not get term and definitions');
+    return Promise.reject("Could not get term and definitions");
   }
 }
 
